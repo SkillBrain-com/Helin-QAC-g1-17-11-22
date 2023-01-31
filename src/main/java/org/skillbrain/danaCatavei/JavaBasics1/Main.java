9eaa30ae6ac186539e121a5ab5dc77662347471a
Danaka72 <dana_catavei@yahoo.com>
2023-01-31 21:08:00 +0200

    commit tema JavaBasics1

---
+++ src/main/java/org/skillbrain/danaCatavei/JavaBasics1/Main.java
@@ -0,0 +1,17 @@
+package org.skillbrain.danaCatavei;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int varsta = 20; // int varsta -> declarare; =20 initializare variabila (dam valoare)
+        String orice = "Orice";
+        System.out.println(orice);
+
+        System.out.println("Am reusit!");
+
+
+    }
+}
---
+++ src/main/java/org/skillbrain/danaCatavei/JavaBasics1/Tema1.java
@@ -0,0 +1,13 @@
+package org.skillbrain.danaCatavei;
+
+public class Tema1{
+    public static void main(String[] args) {
+        exercitiu();
+    }
+
+    static void exercitiu(){
+        System.out.println();
+    }
+}
+
+
