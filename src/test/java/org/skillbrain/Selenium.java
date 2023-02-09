05568c7c786e51ff32b6e10d1625311380cd2136
lucianSimon <luciansimon1964@gmail.com>
2023-02-09 10:18:10 +0200

    modif app

---
+++ src/test/java/org/skillbrain/Selenium.java
@@ -0,0 +1,10 @@
+package org.skillbrain;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+    public static void main(String[] args) {
+       WebDriver driver = new ChromeDriver();
+    }
+}
