b78438bc31b22e7d546a9cd7510398f15f8ea14b
Lucian26 <luciansimon1964@gmail.com>
2023-02-10 17:03:47 +0200

    upgrade chrom 110

--- src/test/java/org/skillbrain/Tema5/Selenium.java
+++ src/test/java/org/skillbrain/Tema5/Selenium.java
@@ -1,4 +1,4 @@
-package org.skillbrain.Tema5;
+package org.skillbrain;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -23,13 +23,13 @@ public class Selenium {
         WebElement textBox=driver.findElement(By.id("item-0")); // fara diez
         textBox.click();
       //  Thread.sleep(2000);
-        driver.findElement(By.id("userName")).sendKeys("LucSim");
+        driver.findElement(By.id("username")).sendKeys("LucSim");
         driver.findElement(By.id("userEmail")).sendKeys("Luc@simmsn");
         driver.findElement(By.id("currentAddress")).sendKeys("Bucuresti");
         driver.findElement(By.id("permanentAddress")).sendKeys("Cluj");
         driver.findElement(By.cssSelector(".btn.btn-primary")).click();
         //Thread.sleep(2000);
-      //  driver.quit();
+        driver.quit();
 
 
 
