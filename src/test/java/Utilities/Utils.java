501b69c60731e77c6af1782d167010cf9c563de3
Danaka72 <dana_catavei@yahoo.com>
2023-02-14 20:34:24 +0200

    Surse actualizate pentru Windows

--- src/test/java/Utilities/Utils.java
+++ src/test/java/Utilities/Utils.java
@@ -7,20 +7,34 @@ import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Utils {
 //    public static void main(String[] args) {
 //        System.out.println(System.getProperty("user.dir"));
 //    }
-    public static void takeScreenshot(WebDriver driver) {
-        try {
-            TakesScreenshot screenshot = (TakesScreenshot) driver;
-            File source = screenshot.getScreenshotAs(OutputType.FILE);
-            String destinationFile = System.getProperty("user.dir") + "printScreen"+ LocalDateTime.now() +".png";
-            FileUtils.copyFile(source, new File(destinationFile));
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+//
+
+    public static void takeScreenshot(WebDriver driver) throws IOException {
+        String destinationFile = null;
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+       // LocalDate today = LocalDate.now();
+        //LocalDateTime today_time = LocalDateTime.now();
+        String DATE_FORMATTER= "yyyy-MM-dd-HHmmss";
+       // System.out.println("Current Date="+today_time);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
+        String formatDateTime = LocalDateTime.now().format(formatter);
+        System.out.println("Formatted Time :" +formatDateTime);
+        //destinationFile = "Logs/printScreen-" + LocalDateTime.now() + ".png";
+        //destinationFile = System.getProperty("user.dir")+"C:\\Users\\stelu\\IdeaProjects\\Helin-QAC-g1-17-11-22\\imagine.png";
+        //FileUtils.copyFile(source, new File("C:\\Users\\stelu\\IdeaProjects\\Helin-QAC-g1-17-11-22\\imagine.png"));
+        int i=1;
+        FileUtils.copyFile(source, new File(System.getProperty("user.dir")+"\\Logs\\print-"+ formatDateTime + ".png"));
+        i+=1;
+
     }
 }
--- src/test/java/org/skillbrain/danaCatavei/AlertTest.java
+++ src/test/java/org/skillbrain/danaCatavei/AlertTest.java
@@ -5,21 +5,24 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+
 public class AlertTest extends Utils {
     private static ChromeDriver driver; // declar fara intilializare
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         driver = new ChromeDriver(); // initializare
 
-        try{
+        try {
             driver.get("https://demoqa.com/alerts");
+            takeScreenshot(driver);
             //driver.findElement(By.id("alertButton")).click();
             //driver.findElement(By.id("confirmButton")).click();
             driver.findElement(By.id("promtButton")).click();
             //schimbati focusulu pe fereastra de alerta
             Alert alert = driver.switchTo().alert();
-            //takeScreenshot(driver);
+
             Thread.sleep(3000);
             // Ok, adica accept
             //alert.accept();
@@ -31,9 +34,9 @@ public class AlertTest extends Utils {
             //System.out.println(driver.findElement(By.id("confirmResult")).getText());
             System.out.println(driver.findElement(By.id("promptResult")).getText());
             Thread.sleep(3000);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-        }finally {
+        } finally {
             driver.quit();
         }
 
