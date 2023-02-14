097334738f8d69e65fcd6a45e02c065b344d896f
skillbraintest <bogdan.mircea@outlook.com>
2023-02-14 19:11:14 +0200

    dupa seleniu 3

--- src/test/java/Utilities/Utils.java
+++ src/test/java/Utilities/Utils.java
@@ -1,24 +1,33 @@
 package Utilities;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
-
 public class Utils {
-    public void takeScreenshot(WebDriver driver) {
-        try {
+    /* public void takeScreenshot (WebDriver driver){
+          try {
+              TakesScreenshot screenshot = (TakesScreenshot) driver;
+              File source = screenshot.getScreenshotAs(OutputType.FILE);
+              String destinationFile = System.getProperty("user.dir") + "/Logs/printScreen-" + LocalDateTime.now() + ".png";
+              FileUtils.copyFile(source, new File(destinationFile));
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+      }*/
+   /* public static void takeScreenshot(WebDriver driver) throws IOException {
+        String destinationFile = null;
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        destinationFile = System.getProperty("user.dir")
+                + "/Logs/printScreen-" + LocalDateTime.now() + ".png";
+        FileUtils.copyFile(source, new File(destinationFile));
+    }*/
+    public static void takeScreenshot(WebDriver driver) throws IOException {
+        String destinationFile = null;
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        //destinationFile = "Logs/printScreen-" + LocalDateTime.now() + ".png";
+        //destinationFile = System.getProperty("user.dir")+"C:\\Users\\stelu\\IdeaProjects\\Helin-QAC-g1-17-11-22\\imagine.png";
+        //FileUtils.copyFile(source, new File(
 
 
-            TakesScreenshot screenshot = (TakesScreenshot) driver;
-            File source = screenshot.getScreenshotAs(OutputType.FILE);
-            String destinationFile = System.getProperty("user.dir") + "/Logs/printScreen-" + LocalDateTime.now() + ".png";
-            FileUtils.copyFile(source, new File(destinationFile));
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
