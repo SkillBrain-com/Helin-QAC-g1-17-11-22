1e8dc73f8de99259d665d177e2a7613abd81f4bb
ale.Alexandra <alexandra.n3107@gmail.com>
2023-02-14 21:46:39 +0200

    update general

---
+++ src/test/java/org/skillbrain/ButtonsTest.java
@@ -0,0 +1,34 @@
+package org.skillbrain;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ButtonsTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/buttons");
+        // test click me
+//        WebElement clickMe = driver.findElement(By.xpath("//button[normalize-space()='Click Me']"));
+//        clickMe.click();
+        // double click me
+//        WebElement doubleCLickMe = driver.findElement(By.id("doubleClickBtn"));
+//        Actions actions = new Actions(driver);
+//        actions.doubleClick().build().perform();
+        // right click me
+        Actions actions = new Actions(driver);
+        WebElement rightClickMe = driver.findElement(By.id("rightClickBtn"));
+        actions.contextClick(rightClickMe).build().perform();
+        String message = driver.findElement(By.tagName("p")).getText();
+        Thread.sleep(3000);
+        System.out.println(message);
+
+
+        driver.quit();
+
+    }
+}
