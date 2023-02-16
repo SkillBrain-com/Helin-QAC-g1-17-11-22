da5c247a7c6a76da2e3b91f4fc72bc60486191ae
Gabriela7-7 <118394027+Gabriela7-7@users.noreply.github.com>
2023-02-16 21:50:38 +0200

    Commit Selenium1punct3.java si Selenium1punctele1si2.java

---
+++ src/main/java/org/skillbrain/Tema4/Selenium1punct3.java
@@ -0,0 +1,57 @@
+package org.skillbrain.Tema4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import javax.xml.xpath.XPathExpression;
+
+public class Selenium1punct3 {
+    public static void main(String[] args) {
+
+        System.setProperty("Chromedriver.chrome.driver"," .idea/drivers/chromedriver.exe");
+        ChromeDriver driver=new ChromeDriver();
+
+
+         driver.get(" https://demoqa.com/text-box");
+         WebElement element=driver.findElement(By.id("app"));
+          System.out.println("app");
+         element.click();
+          WebElement element1=driver.findElement(By.xpath(" //div[@id='fixedban']" ));
+         System.out.println(" //div[@id='fixedban']");
+        WebElement element2=driver.findElement(By.cssSelector("div.body-height "));
+         System.out.println("div.body-height");
+
+         driver.get("https://demoqa.com/checkbox ");
+         WebElement element3=driver.findElement(By.id("fixedban"));
+         System.out.println(" fixedban ");
+         WebElement element4=driver.findElement(By.xpath(" //div[@id='app']"))  ;
+          System.out.println(" //div[@id='app']");
+         WebElement element5=driver.findElement(By.cssSelector("div.container.playgound-body"));
+         System.out.println("div.container.playgound-body");
+
+            driver.get("https://demoqa.com/radio-button ");
+            WebElement element6=driver.findElement(By.cssSelector("div.container.playgound-body "));
+            System.out.println("div.container.playgound-body ");
+            WebElement element7=driver.findElement(By.id("app"));
+            System.out.println("app ");
+            WebElement footer=driver.findElement(By.tagName("footer"));
+             System.out.println("footer");
+             WebElement element8=driver.findElement(By.xpath(" //div[@class='body-height']"));
+             System.out.println("//div[@class='body-height']");
+
+            driver.get(" https://demoqa.com/webtables ");
+
+            WebElement element9=driver.findElement(By.id("app"));
+           System.out.println("app" );
+        WebElement element10=driver.findElement(By.cssSelector("div.container.playgound-body "));
+        System.out.println("div.container.playgound-body ");
+        WebElement element11=driver.findElement(By.id("app"));
+        System.out.println("app ");
+        WebElement element12=driver.findElement(By.xpath(" //div[@class='body-height']"));
+        System.out.println("//div[@class='body-height']");
+
+        driver.quit();
+        }
+
+    }
+
---
+++ src/main/java/org/skillbrain/Tema4/Selenium1punctele1si2.java
@@ -0,0 +1,43 @@
+package org.skillbrain.Tema4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Selenium1punctele1si2 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("Chromedriver.chrome.driver"," .idea/drivers/chromedriver.exe");
+        ChromeDriver driver=new ChromeDriver();
+
+        driver.get("https://testpages.herokuapp.com/styled/index.html ");
+        WebElement element=driver.findElement(By.xpath(" //div[@class='page-body']"));
+        System.out.println(" //div[@class='page-body']");
+          element.click();
+         WebElement element1=driver.findElement(By.cssSelector("div.explanation"));
+         System.out.println("div.explanation");
+         element1.click();
+          driver.get("https://testpages.herokuapp.com/styled/attributes-test.html ");
+         WebElement element2=driver.findElement(By.xpath("//div[@class='explanation']"));
+         System.out.println("//div[@class='explanation']");
+          element2.click();
+         WebElement element3=driver.findElement(By.cssSelector("div.page-body"));
+         System.out.println("div.page-body");
+         element3.click();
+         driver.get("https://testpages.herokuapp.com/styled/find-by-playground-test.html ");
+         WebElement body=driver.findElement(By.tagName("body"));
+         System.out.println(" body");
+         body.click();
+         WebElement element4=driver.findElement(By.id("div1"));
+         System.out.println("div1");
+
+        driver.quit();
+
+
+
+
+    }
+    }
+
