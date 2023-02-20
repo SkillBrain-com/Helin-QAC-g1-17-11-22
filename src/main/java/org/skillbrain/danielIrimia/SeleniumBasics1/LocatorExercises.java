0303bf22c948a5d8cf3f73b15c13b4b209343d43
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-02-20 07:53:49 +0100

    add getGhromeWithWait() methoe to LocatorExercises class

--- src/main/java/org/skillbrain/danielIrimia/SeleniumBasics1/LocatorExercises.java
+++ src/main/java/org/skillbrain/danielIrimia/SeleniumBasics1/LocatorExercises.java
@@ -1,5 +1,15 @@
 package org.skillbrain.danielIrimia.SeleniumBasics1;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class LocatorExercises {
 
+    public static ChromeDriver getGhromeWithWait() {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        return driver;
+    }
 }
