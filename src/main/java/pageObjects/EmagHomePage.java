f488717859486658b4f4fd863d0352eae16ecee8
sandu cristian <sandu_cristian2007@yahoo.com>
2023-02-21 19:13:41 +0200

    add emagHomePage and emagTestPage. Add changes to Utils class

---
+++ src/main/java/pageObjects/EmagHomePage.java
@@ -0,0 +1,57 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmagHomePage {
+
+    private static final String ACCEPT_BUTTON = ".btn.btn-primary.js-accept.gtm_h76e8zjgoo.btn-block";
+    private static final String GDPR_FOOTER =".gdpr-cookie-banner.js-gdpr-cookie-banner.pad-sep-xs.pad-hrz-none.show";
+    private static final String CLOSE_OFERTA_ZILEI ="//button[@class='close']";
+    private static final String INTRARE_IN_CONT = ".js-dismiss-login-notice-btn.dismiss-btn.btn.btn-link.pad-sep-none.pad-hrz-none";
+
+    private static final String INTRARE_CONT_BANNER =".gdpr-cookie-banner.js-gdpr-cookie-banner.pad-sep-xs.pad-hrz-none.login-view.login-view-ro.show";
+
+    private static final String BANNER_INTRARE_BUTTON =".js-dismiss-login-notice-btn.dismiss-btn.btn.btn-link.pad-sep-none.pad-hrz-none";
+    private static final String INTRARE_IN_CONT_BUTTON ="//a[@class='font-size-ms strong login-btn btn btn-primary js-login-btn']";
+    private static final String EMAG_LOGO = "//img[@alt=\"eMAG\"]";
+    private static final String SALUT_MSG ="//*[contains(text(),'Salut!')]";
+
+    @FindBy(xpath = SALUT_MSG)
+    public WebElement salutMsg;
+
+    @FindBy(xpath = EMAG_LOGO)
+    public WebElement emagLogo;
+
+    @FindBy(xpath = INTRARE_IN_CONT_BUTTON)
+    public WebElement intraInContButton;
+
+    @FindBy(css = BANNER_INTRARE_BUTTON)
+    public WebElement bannerCloseButton;
+
+    @FindBy(css = INTRARE_CONT_BANNER)
+    public WebElement loginBanner;
+
+    @FindBy(css = ACCEPT_BUTTON)
+    public WebElement acceptButton;
+
+    @FindBy(css = GDPR_FOOTER )
+    public WebElement gdprFooter;
+
+    @FindBy(xpath = CLOSE_OFERTA_ZILEI)
+    public WebElement closeOfertaZileiButton;
+
+    @FindBy(css = INTRARE_IN_CONT)
+    public WebElement intraInCont;
+
+
+    public EmagHomePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+
+
+
+}
--- src/test/java/Utilities/Utils.java
+++ src/test/java/Utilities/Utils.java
@@ -4,12 +4,30 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Utils {
+
+
+    public static WebDriver getChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+//        options.setHeadless(true);   browser invizibil
+        System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver(options);  // initilizare
+//        driver.manage().window().maximize();
+        // setam un wait global (implicit)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+
+        return driver;
+    }
 
     public static void takeScreenshot(WebDriver driver) throws IOException {
         String destinationFile = null;
