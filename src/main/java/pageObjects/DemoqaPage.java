1bfd79423351a88b29a707864fb057a34e94f937
Danaka72 <dana_catavei@yahoo.com>
2023-02-21 20:56:49 +0200

    Selenium3 demoqaPage clasa

---
+++ src/main/java/pageObjects/DemoqaPage.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DemoqaPage {
+
+    private final static String BANNER_IMAGE = ".banner-image";
+    private final static String GO_TO_REGISTRATION ="//a[normalize-space()='Go To Registration']";
+
+
+    private final static String OPTION_ROMANIA ="option[value='70']";
+
+    @FindBy(css=BANNER_IMAGE)
+    public WebElement bannerImage;
+
+    @FindBy(xpath=GO_TO_REGISTRATION)
+   // @FindBy(css=GO_TO_REGISTRATION)
+    public WebElement registrationButton;
+    @FindBy(css=OPTION_ROMANIA)
+    public WebElement optionRO;
+
+    public DemoqaPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+}
