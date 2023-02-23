b5c86dd104902ed674d5bd905c264c93fb7aa0b5
Gabriela7-7 <118394027+Gabriela7-7@users.noreply.github.com>
2023-02-23 19:35:35 +0200

    Commit Selenium1punct3.java si Selenium1punctele1si2.java Commit Tema 1

---
+++ src/main/java/PageObjects/EmagHomeTest.java
@@ -0,0 +1,28 @@
+package PageObjects;
+import PageObjects.EmagHomePage;
+import Utilites.Utils;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+public class EmagHomeTest {
+@BeforeTest
+public void emagPageTest
+
+
+
+
+
+
+
+
+    public  EmagHomeTest  (WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+    public static void main(String[] args) {
+
+}}
