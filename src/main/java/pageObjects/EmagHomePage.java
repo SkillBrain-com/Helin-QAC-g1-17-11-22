d2b3a5527f2e1d3761c6bcc98361af693fb7d4f2
ale.Alexandra <alexandra.n3107@gmail.com>
2023-02-26 14:48:55 +0200

    update EmagTest, EmagHomepage,Exceptiontest, AppTest

---
+++ src/main/java/pageObjects/EmagHomePage.java
@@ -0,0 +1,79 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmagHomePage {
+
+    private static final String ACCEPT_BUTTON = ".btn.btn-primary.js-accept.gtm_h76e8zjgoo.btn-block";
+    private static final String GDPR_FOOTER =".gdpr-cookie-banner.js-gdpr-cookie-banner.pad-sep-xs.pad-hrz-none.show";
+    private static final String CLOSE_OFERTA_ZILEI ="//button[@class='close']";
+    private static final String INTRARE_IN_CONT = ".js-dismiss-login-notice-btn.dismiss-btn.btn.btn-link.pad-sep-none.pad-hrz-none";
+    private static final String INTRARE_CONT_BANNER =".gdpr-cookie-banner.js-gdpr-cookie-banner.pad-sep-xs.pad-hrz-none.login-view.login-view-ro.show";
+    private static final String BANNER_INTRARE_BUTTON =".js-dismiss-login-notice-btn.dismiss-btn.btn.btn-link.pad-sep-none.pad-hrz-none";
+    private static final String INTRARE_IN_CONT_BUTTON ="//a[@class='font-size-ms strong login-btn btn btn-primary js-login-btn']";
+    private static final String EMAG_LOGO = "//img[@alt=\"eMAG\"]";
+    private static final String SALUT_MSG ="//*[contains(text(),'Salut!')]";
+    private static final String EMAIL ="user_login_email";
+    private static final String LOGIN_BUTTON ="user_login_continue";
+    private static final String PASSWORD = "user_login_password";
+    private static final String CONTINUE_LOGIN = "user_login_continue";
+    private static final String LOGIN_ERROR ="//*[contains(text(),'Ai introdus greșit parola sau adresa de email. Te rugăm completează din nou.')]";
+
+    private static final String PASSWORD_HEADER ="//label[@for='user_login_password']";
+
+    @FindBy(xpath = PASSWORD_HEADER)
+    public WebElement passwordHeader;
+
+    @FindBy(xpath = LOGIN_ERROR)
+    public WebElement loginErrorMessage;
+
+    @FindBy(id = CONTINUE_LOGIN)
+    public WebElement continueButton;
+
+    @FindBy(id = PASSWORD)
+    public WebElement password;
+    @FindBy(id = LOGIN_BUTTON)
+    public WebElement loginButton;
+
+    @FindBy(id = EMAIL)
+    public WebElement email;
+    @FindBy(xpath = SALUT_MSG)
+    public WebElement salutMsg;
+
+    @FindBy(xpath = EMAG_LOGO)
+    public WebElement emagLogo;
+
+    @FindBy(xpath = INTRARE_IN_CONT_BUTTON)
+    public WebElement intraInContButton;
+
+    @FindBy(css = BANNER_INTRARE_BUTTON)
+    public WebElement bannerCloseButton;
+
+    @FindBy(css = INTRARE_CONT_BANNER)
+    public WebElement loginBanner;
+
+    @FindBy(css = ACCEPT_BUTTON)
+    public WebElement acceptButton;
+
+    @FindBy(css = GDPR_FOOTER )
+    public WebElement gdprFooter;
+
+    @FindBy(xpath = CLOSE_OFERTA_ZILEI)
+    public WebElement closeOfertaZileiButton;
+
+    @FindBy(css = INTRARE_IN_CONT)
+    public WebElement intraInCont;
+
+
+
+    public EmagHomePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+
+
+
+}
---
+++ src/test/java/org/skillbrain/AppTest.java
@@ -0,0 +1,36 @@
+package org.skillbrain;
+
+import Utilities.Utils;
+import org.openqa.selenium.WebDriver;
+import pageObjects.DemoqaPage;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class AppTest extends Utils {
+
+    public static void main(String[] args) {
+        WebDriver driver = getChromeDriver();
+        try {
+            driver.get("https://demoqa.com/");
+            DemoqaPage demoqaPage = new DemoqaPage(driver);
+            demoqaPage.bannerImage.click();
+            Set<String> tabIDs = driver.getWindowHandles();
+            System.out.println(tabIDs.size());
+            Iterator<String> iter = tabIDs.iterator();
+            String parentid = iter.next();
+            String childId = iter.next();
+            driver.switchTo().window(childId);
+            demoqaPage.goToRegistrationButton.click();
+            demoqaPage.optionRO.click();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+
+    }
+
+}
