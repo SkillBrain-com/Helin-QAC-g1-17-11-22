40d9a00f7100461c87b374a00defea217d12d6ef
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-02-28 17:18:21 +0100

    modified TestpagesHerokuapp.java

--- src/test/java/org/skillbrain/danielIrimia/SeleniumBasics1/TestpagesHerokuapp.java
+++ src/test/java/org/skillbrain/danielIrimia/SeleniumBasics1/TestpagesHerokuapp.java
@@ -1,5 +1,8 @@
 package org.skillbrain.danielIrimia.SeleniumBasics1;
 
+import org.openqa.selenium.WebDriver;
+
 public class TestpagesHerokuapp extends LocatorExercises {
+    WebDriver driver = LocatorExercises.getGhromeWithWait();
 
 }
