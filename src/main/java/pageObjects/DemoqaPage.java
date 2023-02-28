37b3c96c6f9c3471e30ca3af17987aed89d96a45
Danaka72 <dana_catavei@yahoo.com>
2023-02-28 19:22:00 +0200

    add AT101 - create new TestCase

--- src/main/java/pageObjects/DemoqaPage.java
+++ src/main/java/pageObjects/DemoqaPage.java
@@ -13,6 +13,12 @@ public class DemoqaPage {
 
     private final static String OPTION_ROMANIA ="option[value='70']";
 
+    private final static String ALERT_PANEL = "(//div[@class='card mt-4 top-card'])[3]";
+
+    private final static String ALERT_BUTTON = "(//li[@id='item-1'])[2]";
+
+    private final static String  CLICK_ME_1 = "alertButton";
+
     @FindBy(css=BANNER_IMAGE)
     public WebElement bannerImage;
 
@@ -21,6 +27,14 @@ public class DemoqaPage {
     public WebElement registrationButton;
     @FindBy(css=OPTION_ROMANIA)
     public WebElement optionRO;
+
+    @FindBy(xpath = ALERT_PANEL)
+    public WebElement alertPanelButton;
+
+    @FindBy(xpath = ALERT_BUTTON)
+    public WebElement alertButton;
+    @FindBy(id = CLICK_ME_1)
+    public WebElement clickMeButton_1;
 
     public DemoqaPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
