72c91ede971e125e8ba5b73aa3a8086eb2329119
Lucian26 <luciansimon1964@gmail.com>
2023-03-02 21:21:19 +0200

    add tema7

--- src/test/java/Utilities/Utils.java
+++ src/test/java/Utilities/Utils.java
@@ -1,30 +1,52 @@
 package Utilities;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Utils {
 
+  //  public static RemoteWebDriver getRemoteWebDriver() throws MalformedURLException {
+   //     return new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
 
+    //    public static void main(String[] args) {
+//        System.out.println(System.getProperty("user.dir"));
+//    }
+//
+    public static WebDriver getChromeDriver(){
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+        //setam un wait global (implicit)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        return driver;
+    }
     public static void takeScreenshot(WebDriver driver) throws IOException {
         String destinationFile = null;
         TakesScreenshot screenshot = (TakesScreenshot) driver;
+
         File source = screenshot.getScreenshotAs(OutputType.FILE);
-        String DATE_FORMATTER = "yyyy-MM-dd-HHmmss";
+        // LocalDate today = LocalDate.now();
+        //LocalDateTime today_time = LocalDateTime.now();
+        String DATE_FORMATTER= "yyyy-MM-dd-HHmmss";
+        // System.out.println("Current Date="+today_time);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
         String formatDateTime = LocalDateTime.now().format(formatter);
-        System.out.println("Formatted Time :" + formatDateTime);
-        int i = 1;
-        FileUtils.copyFile(source, new File(System.getProperty("user.dir") + "\\Logs\\print-" + formatDateTime + ".png"));
-        i += 1;
+        System.out.println("Formatted Time :" +formatDateTime);
+        //destinationFile = "Logs/printScreen-" + LocalDateTime.now() + ".png";
+        //destinationFile = System.getProperty("user.dir")+"C:\\Users\\stelu\\IdeaProjects\\Helin-QAC-g1-17-11-22\\imagine.png";
+        //FileUtils.copyFile(source, new File("C:\\Users\\stelu\\IdeaProjects\\Helin-QAC-g1-17-11-22\\imagine.png"));
+        int i=1;
+        FileUtils.copyFile(source, new File(System.getProperty("user.dir")+"\\Logs\\print-"+ formatDateTime + ".png"));
+        i+=1;
 
     }
 }
-
-
-
