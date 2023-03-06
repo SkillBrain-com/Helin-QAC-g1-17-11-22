2d9d8f5d31020d8b095b5c9d52e44735262a6bf9
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-03-06 17:01:09 +0100

    added EmagHomePage.java

---
+++ src/test/java/org/skillbrain/danielIrimia/EmagTest.java
@@ -0,0 +1,41 @@
+package org.skillbrain.danielIrimia;
+
+import Utilities.Utils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObjects.EmagHomePage;
+
+import java.time.Duration;
+
+public class EmagTest extends Utils {
+
+
+
+    public static void main(String[] args) {
+        WebDriver driver = getChromeDriver();
+        try {
+            driver.get("https://www.emag.ro/");
+            EmagHomePage homePage = new EmagHomePage(driver);
+            //homePage.closeOfertaZileiButton.click();
+            homePage.gdprFooter.click();
+            homePage.acceptButton.click();
+            driver.navigate().refresh();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOf(homePage.gdprFooter)); // 2 s
+            homePage.acceptButton.click();
+            wait.until(ExpectedConditions.visibilityOf(homePage.loginBanner));
+            homePage.intraInContButton.click();
+            wait.until(ExpectedConditions.visibilityOf(homePage.emagLogo));
+            System.out.println(homePage.salutMsg.getText());
+            // alerts, file uploads, modals - de gasit exemple
+            System.out.println("Skillbrain va saluta!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+
+
+    }
+}
