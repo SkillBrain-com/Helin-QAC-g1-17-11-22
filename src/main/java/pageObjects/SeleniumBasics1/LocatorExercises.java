5a27dcbf8ce8dcebac2246f439b21927f0306335
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-03-07 17:38:01 +0100

    modified LocatorExercises.java

--- src/main/java/pageObjects/SeleniumBasics1/LocatorExercises.java
+++ src/main/java/pageObjects/SeleniumBasics1/LocatorExercises.java
@@ -1,17 +1,6 @@
 package pageObjects.SeleniumBasics1;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
-
 public class LocatorExercises {
-
-    public static ChromeDriver getGhromeWithWait() {
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        return driver;
-    }
 
 
 }
