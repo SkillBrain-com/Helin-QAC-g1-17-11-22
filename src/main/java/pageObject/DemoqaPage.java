019eb5c9be433a8426bdd2d4876196b49f906c3a
PanaitStelian <spanait260@gmail.com>
2023-03-09 21:48:37 +0200

    AT115 adapt testcases

--- src/main/java/pageObject/DemoqaPage.java
+++ src/main/java/pageObject/DemoqaPage.java
@@ -7,22 +7,35 @@ import org.openqa.selenium.support.PageFactory;
 
 public class DemoqaPage {
 
-    private final static String BANNER_IMAGE =".banner-image";
-    private final static String GO_TO_REGISTRATION ="//a[normalize-space()='Go To Registration']";
-    private final static String OPTION_ROMANIA ="option[value='70']";
-    private final static String ALERT_PANEL ="(//div[@class='card mt-4 top-card'])[3]";
-    private final static String ALERT_BUTTON ="(//li[@id='item-1'])[2]";
-    private final static String CLIC_KME_1  ="alertButton";
-    private final static String WINDOW_PANEL="//h5[normalize-space()='Alerts, Frame & Windows']";
-    private static final String WINDOW_BUTTON ="//span[normalize-space()='Browser Windows']";
-   private static final String NEW_TAB_BUTTON ="//button[@id='tabButton']";
-   private static final String PAGE_MESSAGE ="sampleHeading";
+    private final static String BANNER_IMAGE = ".banner-image";
+    private final static String GO_TO_REGISTRATION = "//a[normalize-space()='Go To Registration']";
+    private final static String OPTION_ROMANIA = "option[value='70']";
+    private final static String ALERT_PANEL = "(//div[@class='card mt-4 top-card'])[3]";
+    private final static String ALERT_BUTTON = "(//li[@id='item-1'])[2]";
+    private final static String CLIC_KME_1 = "alertButton";
+    private final static String WINDOW_PANEL = "//h5[normalize-space()='Alerts, Frame & Windows']";
+    private static final String WINDOW_BUTTON = "//span[normalize-space()='Browser Windows']";
+    private static final String NEW_TAB_BUTTON = "//button[@id='tabButton']";
+    private static final String PAGE_MESSAGE = "sampleHeading";
+
+    private static final String ELEMENTS_CARD = "(//div[@class='card mt-4 top-card'])[1]";
+    private static final String RADIO_BUTTON = "(//span[normalize-space()='Radio Button'])[1]";
+    private static final String YES_BUTTON = "label[for='yesRadio']";
+    private static final String IMPRESSIVE_BUTTON = "//label[normalize-space()='Impressive']";
 
 
-   @FindBy(id = PAGE_MESSAGE)
-   public WebElement pageMessage;
-   @FindBy(xpath = NEW_TAB_BUTTON)
-   public WebElement newTabButton;
+    @FindBy(xpath = IMPRESSIVE_BUTTON)
+    public WebElement impressiveButton;
+    @FindBy(css = YES_BUTTON)
+    public WebElement yesButton;
+    @FindBy(xpath = RADIO_BUTTON)
+    public WebElement radioButton;
+    @FindBy(xpath = ELEMENTS_CARD)
+    public WebElement elementCard;
+    @FindBy(id = PAGE_MESSAGE)
+    public WebElement pageMessage;
+    @FindBy(xpath = NEW_TAB_BUTTON)
+    public WebElement newTabButton;
     @FindBy(xpath = WINDOW_BUTTON)
     public WebElement windowButton;
 
@@ -33,20 +46,19 @@ public class DemoqaPage {
 
     @FindBy(xpath = ALERT_BUTTON)
     public WebElement alertButton;
-    @FindBy(xpath =ALERT_PANEL )
+    @FindBy(xpath = ALERT_PANEL)
     public WebElement alertPanelButton;
 
     @FindBy(css = OPTION_ROMANIA)
     public WebElement optionRomania;
     @FindBy(css = BANNER_IMAGE)
     public WebElement bannerImage;
-   @FindBy(css = GO_TO_REGISTRATION)
-   public WebElement goToRegistrationButton;
-    public DemoqaPage(WebDriver driver){
-        PageFactory.initElements(driver,this);
+    @FindBy(css = GO_TO_REGISTRATION)
+    public WebElement goToRegistrationButton;
+
+    public DemoqaPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
-
-
 
 
 }
---
+++ src/test/java/org/skillbrain/AT115.java
@@ -0,0 +1,48 @@
+package org.skillbrain;
+
+import Utilities.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pageObject.DemoqaPage;
+
+public class AT115 extends Utils {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void startTest(){
+        driver = getChromeDriver();
+
+
+    }
+    @AfterTest
+    public void stopTest(){
+        driver.quit();
+    }
+
+    @Test
+    public void RadiobuttonTest(){
+        driver.get("https://demoqa.com/");
+        DemoqaPage page = new DemoqaPage(driver);
+        page.elementCard.click();
+        page.radioButton.click();
+        page.yesButton.click();
+        String yesMessage = driver.findElement(By.cssSelector(".mt-3")).getText();
+        Assert.assertEquals(yesMessage, "You have selected Yes");
+        takeScreenshot(driver);
+        page.impressiveButton.click();
+        String impressiveMessage = driver.findElement(By.cssSelector(".mt-3")).getText();
+        Assert.assertEquals(impressiveMessage, "You have selected Impressive");
+
+
+    }
+
+
+
+
+
+}
