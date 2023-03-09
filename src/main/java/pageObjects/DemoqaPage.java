6f1ec1a5504970f7ce97c6a7a52c7638facd50b5
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-03-09 17:47:35 +0100

    modified DemoqaPage

--- src/main/java/pageObjects/DemoqaPage.java
+++ src/main/java/pageObjects/DemoqaPage.java
@@ -4,61 +4,62 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.w3c.dom.html.HTMLInputElement;
 
 public class DemoqaPage {
 
-    private final static String BANNER_IMAGE = ".banner-image";
+    // WebDriver driver=new ChromeDriver();
+
+    private final static String BANNER_IMAGE=".banner-image";
     private final static String GO_TO_REGISTRATION ="//a[normalize-space()='Go To Registration']";
 
-
     private final static String OPTION_ROMANIA ="option[value='70']";
+    // private final static String GO_TO_REGISTRATION =".btn.btn-primary.shadow.btn.block";
+    private final static String WINDOW_PANEL="//h5[normalize-space()='Alerts, Frame & Windows']";
+    private final static String WINDOW_BUTTON=" //span[normalize-space()='Browser Windows']";
+    private  final static String NEW_TAB_BUTTON="//button[@id='tabButton']";
+    private final static String PAGE_MESAGE="sampleHeading";
 
-    private final static String ALERT_PANEL = "(//div[@class='card mt-4 top-card'])[3]";
+    private final static String ALERT_PANEL_BUTTON="(//div)[27]";
+    private final static String  ALERT_BUTTON="(//li[@id='item-1'])[2]";
+    private final static String  CLICKME_BUTTON_1="alertButton";
 
-    private final static String ALERT_BUTTON = "(//li[@id='item-1'])[2]";
-
-    private final static String  CLICK_ME_1 = "alertButton";
-
-    // private final static String WINDOW_PANEL = "(//div[contains(@class,'header-text')][normalize-space()='Alerts, Frame & Windows'])[1]";
-    private final static String WINDOW_BUTTON= "(//div[@class='card mt-4 top-card'])[3]";
+    // private final static String WINDOW_BUTTON= "//div[contains(@class,'header-text')][normalize-space()='Alerts, Frame & Windows']";
+    // private final static String WINDOW_BUTTON= "(//div[@class='card mt-4 top-card'])[3]";
     private final static String  BROWSER_WINDOW_BUTTON = "(//li[@id='item-0'])[3]";
     private final static String NEWTAB = "tabButton";
-    private final static String MESSAGE = "sampleHeading";
 
-    private final static String NEW_TAB_MESSAGE = "messageWindowButton";
 
-    @FindBy(css=BANNER_IMAGE)
-    public WebElement bannerImage;
+    @FindBy(xpath =ALERT_PANEL_BUTTON)
+    public WebElement alertpanelbutton;
+    @FindBy(xpath =ALERT_BUTTON)
+    public WebElement alertbutton;
+    @FindBy(id = CLICKME_BUTTON_1)
+    public WebElement clickMebutton_1;
 
-    @FindBy(xpath=GO_TO_REGISTRATION)
-    // @FindBy(css=GO_TO_REGISTRATION)
-    public WebElement registrationButton;
-    @FindBy(css=OPTION_ROMANIA)
-    public WebElement optionRO;
-
-    @FindBy(xpath = ALERT_PANEL)
-    public WebElement alertPanelButton;
-
-    @FindBy(xpath = ALERT_BUTTON)
-    public WebElement alertButton;
-    @FindBy(id = CLICK_ME_1)
-    public WebElement clickMeButton_1;
-    //    @FindBy(xpath = WINDOW_PANEL)
-//    public WebElement clickMeButton_1;
+    @FindBy(xpath = WINDOW_PANEL)
+    public  WebElement windowPanel;
     @FindBy(xpath = WINDOW_BUTTON)
-    public WebElement clickWindowButton;
+    public WebElement windowButton;
+    @FindBy(xpath =NEW_TAB_BUTTON )
+    public WebElement newtabbutton;
+    @FindBy(id =PAGE_MESAGE )
+    public WebElement pagemesage;
 
-    @FindBy(xpath = BROWSER_WINDOW_BUTTON)
-    public WebElement clickBrowserWindowButton;
-    @FindBy(id = NEWTAB)
-    public WebElement newTabButton;
+    @FindBy(css= " BANNER_IMAGE")
+    public   WebElement bannerImage;
+    @FindBy(xpath = "GO_TO_REGISTRATION")
+    // @FindBy(css =" GO_TO_REGISTRATION")
+    public   WebElement gotoregistrationButton;
+    @FindBy(xpath = "OPTION_ROMANIA")
+    public   WebElement optionRo;
+    public HTMLInputElement clickwindowButton;
+    public HTMLInputElement clickBrowserWindowButton;
+    public HTMLInputElement newTabMessageButton;
 
-    @FindBy(id = MESSAGE)
-    public WebElement messageBrowser;
-    public DemoqaPage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
+    //  @FindBy(css=BANNER_IMAGE)
+    //  public WebElement bannerImage;
+    public DemoqaPage(WebDriver driver ){
+        PageFactory.initElements(driver,this);
     }
-
-    @FindBy(id=NEW_TAB_MESSAGE)
-    public WebElement newTabMessageButton;
 }
