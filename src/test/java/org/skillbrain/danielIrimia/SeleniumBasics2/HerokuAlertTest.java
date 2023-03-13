6718de7b6fe769c13a7bbf05e1386d06deb85f2b
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-03-13 07:30:29 +0100

    added HerokuAlertTest.java

---
+++ src/test/java/org/skillbrain/danielIrimia/SeleniumBasics2/HerokuAlertTest.java
@@ -0,0 +1,54 @@
+package org.skillbrain.danielIrimia.SeleniumBasics2;
+
+import Utilities.Utils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pageObjects.SeleniumBasics1.HerokuApp;
+
+import java.io.IOException;
+
+public class HerokuAlertTest extends Utils {
+    WebDriver driver;
+    HerokuApp page;
+    private final static String APP_URL = "https://testpages.herokuapp.com/styled/index.html";
+
+    @BeforeTest
+    public void startTest() {
+        driver = getChromeDriver();
+        page = new HerokuApp(driver);
+        driver.get(APP_URL);
+    }
+
+    @Test
+    public void buttonsAlertTest() throws IOException {
+        Actions actions = new Actions(driver);
+        try {
+            page.alertTestPage.click();
+            actions.moveToElement(page.alertButton).click().perform();
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+            takeScreenshot(driver);
+            actions.moveToElement(page.alertButtonConfirm).click().perform();
+            alert = driver.switchTo().alert();
+            alert.dismiss();
+            System.out.println(driver.findElement(By.id("confirmexplanation")).getText());
+            actions.moveToElement(page.alertButtonPrompt).click().perform();
+            alert = driver.switchTo().alert();
+            alert.sendKeys("Selenium Test");
+            alert.accept();
+            System.out.println(driver.findElement(By.id("promptexplanation")).getText());
+            // element do not exist on the alert page
+            System.out.println(driver.findElement(By.id("fakealert")).getText());
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+            takeScreenshot(driver);
+        } finally {
+            driver.quit();
+        }
+    }
+
+}
