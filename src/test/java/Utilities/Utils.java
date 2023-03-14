a1b5b94efbd1ab7bd7172fa8a6e0dd26a6c3c8f0
Luca Cristian <prof_cristianluca@hotmail.com>
2023-03-14 22:02:53 +0200

    modificari

--- src/test/java/Utilities/Utils.java
+++ src/test/java/Utilities/Utils.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,8 +19,11 @@ public class Utils {
 //    }
 //
     public static WebDriver getChromeDriver(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         //driver.manage().window().maximize();
         //setam un wait global (implicit)
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
