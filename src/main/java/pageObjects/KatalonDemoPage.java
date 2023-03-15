789907c18c06cb7ce8b556e4b6b06a8a7b801a93
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-03-15 21:21:20 +0100

    adde TestKatalonDemoPage.java

---
+++ src/main/java/pageObjects/KatalonDemoPage.java
@@ -0,0 +1,97 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class KatalonDemoPage {
+
+
+    private final static String BTN_MAKE_APPOINTMENT = "btn-make-appointment";
+    private final static String MENU_TOGGLE = "menu-toggle";
+    private final static String HEALTHCARE_LINK ="//a[normalize-space()='CURA Healthcare']";
+    private final static String HOME = "//a[normalize-space()='Home']";
+    private final static String LOGIN_MENU = "//a[normalize-space()='Login']";
+    private final static String FORM = "form";
+
+    private final static String USERNAME = "txt-username";
+    private final static String PASSWORD = "txt-password";
+    private final static String LOGIN_BUTTON_FORM = "btn-login";
+    private final static String LOGIN_ERROR_MESSAGE = "//p[@class='lead text-danger']";
+    private final static String SELECT_COMBO_FACILITY= "combo_facility";
+    private final static String SELECT_COMBO_FACILITY_TOKYO = "option[value='Tokyo CURA Healthcare Center']";
+    private final static String SELECT_COMBO_FACILITY_HONGKONG = "//option[@value='Hongkong CURA Healthcare Center']";
+    private final static String SELECT_COMBO_FACILITY_SEOUL = "//option[@value='Seoul CURA Healthcare Center']";
+    private final static String CHECKBOX_HOSPITAL_READMISSION = "chk_hospotal_readmission";
+    private final static String RADIO_PROGRAM_MEDICARE = "radio_program_medicare";
+    private final static String RADIO_PROGRAM_MEDICAID = "radio_program_medicaid";
+    private final static String RADIO_PROGRAM_NONE = "radio_program_none";
+    private final static String TXT_VISIT_DATE = "txt_visit_date";
+    private final static String DAY_28 = "//td[@class='day'][normalize-space()='28']";
+    private final static String APPOINTMENT_TXT_COMMENT = "txt_comment";
+    private final static String BTN_BOOK_APPOINTMENT = "btn-book-appointment";
+    private final static String APPOINTMENT_CONFIRMATION = "//section[@id='summary']//div[@class='row']";
+    private final static String GO_TO_HOMEPAGE = "//a[normalize-space()='Go to Homepage']";
+    ////a[@href='authenticate.php?logout']
+    private final static String LOGOUT_MENU = "//a[@href='authenticate.php?logout']";
+    private final static String LOGOUT_BTN = "(//a[@class='btn btn-default'])[1]";
+
+    @FindBy(xpath = LOGOUT_BTN)
+    public WebElement logoutBtn;
+    @FindBy(xpath = LOGOUT_MENU)
+    public WebElement logoutMenu;
+    @FindBy(xpath = GO_TO_HOMEPAGE)
+    public WebElement goToHomepage;
+    @FindBy(xpath = APPOINTMENT_CONFIRMATION)
+    public WebElement appointmentConfirmation;
+    @FindBy(id = BTN_BOOK_APPOINTMENT)
+    public WebElement btnBookAppointment;
+    @FindBy(id = APPOINTMENT_TXT_COMMENT)
+    public WebElement appointmentTxtComment;
+    @FindBy(xpath = DAY_28)
+    public WebElement day28;
+    @FindBy(id = TXT_VISIT_DATE)
+    public WebElement txtVisitDate;
+    @FindBy(xpath = SELECT_COMBO_FACILITY_HONGKONG)
+    public WebElement selectComboFacilityHongkong;
+    @FindBy(xpath = SELECT_COMBO_FACILITY_SEOUL)
+    public WebElement selectComboFacilitySeoul;
+    @FindBy(xpath = SELECT_COMBO_FACILITY_TOKYO)
+    public WebElement selectComboFacilityTokyo;
+    @FindBy(id = RADIO_PROGRAM_NONE)
+    public WebElement radioProgramNone;
+    @FindBy(id = RADIO_PROGRAM_MEDICAID)
+    public WebElement radioProgramMedicaid;
+    @FindBy(id = RADIO_PROGRAM_MEDICARE)
+    public WebElement radioProgramMedicare;
+    @FindBy(id = CHECKBOX_HOSPITAL_READMISSION)
+    public WebElement checkboxHospitalReadmission;
+    @FindBy(id = SELECT_COMBO_FACILITY)
+    public WebElement selectComboFacility;
+    @FindBy(xpath = LOGIN_ERROR_MESSAGE)
+    public WebElement loginErrorMessage;
+    @FindBy(id = LOGIN_BUTTON_FORM)
+    public WebElement loginButtonForm;
+    @FindBy(id = PASSWORD)
+    public WebElement password;
+    @FindBy(id = USERNAME)
+    public WebElement userName;
+    @FindBy(tagName = FORM)
+    public WebElement form;
+    @FindBy(xpath = HEALTHCARE_LINK)
+    public WebElement healthcareLink;
+    @FindBy(xpath = LOGIN_MENU)
+    public WebElement loginMenu;
+    @FindBy(xpath = HOME)
+    public WebElement home;
+    @FindBy(id = MENU_TOGGLE)
+    public WebElement menuToggle;
+    @FindBy(id = BTN_MAKE_APPOINTMENT)
+    public WebElement btnMakeAppointment;
+
+    public KatalonDemoPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+}
---
+++ src/test/java/org/skillbrain/danielIrimia/TestNG/TestKatalonDemoPage.java
@@ -0,0 +1,89 @@
+package org.skillbrain.danielIrimia.TestNG;
+
+import Utilities.Utils;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pageObjects.KatalonDemoPage;
+
+import java.io.IOException;
+import java.time.Duration;
+public class TestKatalonDemoPage extends Utils{
+    WebDriver driver;
+    KatalonDemoPage page;
+    public String APP_URL = "https://katalon-demo-cura.herokuapp.com/";
+    @DataProvider(name = "loginData")
+    public Object[][] loginData(){
+        return new Object[][]{
+                {"John Doe","ThisIsNotAPassword", "validCredential"},
+                {"","ThisIsNotAPassword", "invalidCredential"}
+        };
+    }
+
+    @BeforeTest
+    public void startTest(){
+        driver = getChromeDriver();
+        page = new KatalonDemoPage(driver);
+        driver.get(APP_URL);
+    }
+
+
+
+
+    // 2.1. Login from the menu in the upper right corner, with a valid username and password
+    @Test(dataProvider = "loginData")
+    public void loginTest(String username, String password,String credentialType) throws IOException {
+        driver.get(APP_URL);
+        page.menuToggle.click();
+        page.loginMenu.click();
+        Assert.assertTrue(page.form.isDisplayed());
+        page.userName.sendKeys(username);
+        page.password.sendKeys(password);
+        page.loginButtonForm.submit();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        if(credentialType.equals("validCredential")){
+            Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/#appointment");
+            Assert.assertTrue(page.btnMakeAppointment.isDisplayed());
+            page.menuToggle.click();
+            page.logoutMenu.click();
+            Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/");
+            takeScreenshot(driver);
+            System.out.println("Login with valid credential Test passed");
+        } else if (credentialType.equals("invalidCredential")) {
+            page.userName.sendKeys(username);
+            page.password.sendKeys(password);
+            Assert.assertTrue(page.loginErrorMessage.isDisplayed());
+            System.out.println(page.loginErrorMessage.getText());
+            Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/profile.php#login");
+            Assert.assertTrue(page.loginButtonForm.isDisplayed());
+            takeScreenshot(driver);
+            System.out.println("Login with invalid credential Test passed");
+        }
+
+    }
+
+    // 2.2. Login from the menu in the upper right corner, with an invalid username and password
+//    @Test(dataProvider = "loginData")
+//    public void loginWithInvalidCredentialTest(String username, String password,String credentialType){
+//        driver.get(APP_URL);
+//        page.menuToggle.click();
+//        page.loginMenu.click();
+//        Assert.assertTrue(page.form.isDisplayed());
+//        page.userName.sendKeys(username);
+//        page.password.sendKeys(password);
+//        page.loginButtonForm.submit();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        if(credentialType.equals("invalidCredential")){
+//            page.loginErrorMessage.isDisplayed();
+//            Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/#login");
+//            Assert.assertTrue(page.loginButtonForm.isDisplayed());
+//            System.out.println("Login with invalid credential Test passed");
+//        }
+//    }
+
+
+    @AfterTest
+    public void endTest(){
+        driver.quit();
+    }
+}
