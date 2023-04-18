9d8c71fe4793e8a01f72a9506afe3df0d9c1d2a7
anda <darjan_anda@yahoo.com>
2023-04-18 22:04:23 +0300

    add Tema3

--- src/main/java/org/skillbrain/andaDarjan/Selenium/Selenium.java
+++ src/main/java/org/skillbrain/andaDarjan/Selenium/Selenium.java
@@ -5,14 +5,57 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 
 public class Selenium {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //Unde se afla chromedriver?
         System.setProperty("webdriver.chrome.driver", "drivers/chromedrv.exe");
         WebDriver driver = new ChromeDriver();
+
+        //pt maximizarea ferestrei
+        driver.manage().window().maximize();
+
+        //accesare pag web
+        WebDriver driver = new ChromeDriver();
+
         driver.get("https://demoqa.com/");
+        WebElement header = driver.findElement(By.tagName("header"));
+        WebElement elements = driver.findElement(By.cssSelector(".card.mt-4.top-card"));
+        elements.click();
+        WebElement textBox = driver.findElement(By.id("item-0")); // Fara #!!!
+        textBox.click();
+        driver.findElement(By.id("userName")).sendKeys("cristian sandu");
+        driver.findElement(By.id("userEmail")).sendKeys("email@email.com");
+        driver.findElement(By.id("currentAdress")).sendKeys("Home");
+        driver.findElement(By.id("permanentAddress")).sendKeys("Home");
+        //optional
+        WebElement submitButton = driver.findElement(By.cssSelector(".btn.btn-primary"));
+
+        List<WebElement> mbElements = driver.findElements(By.tagName("p"));
+        for (int i = 0; i< mbElements).size(); i++) {
+            System.out.println(mbElements.get(i).getText());
+        }
+        driver.findElement(By.xpath("(//li[@id='item-1'])[1]")).click();
+        driver.findElement(By.cssSelector(".rct-icon.rct-icon-expand-close")).click();
+        driver.findElement(By.xpath("//label[@for='tree-node-desktop']" +
+                "//span[@class='rct-checkbox']//*[name()='svg']")).click();
+        System.out.println(driver.findElement(By.id("result")).getText());
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//li[@id='item-2'])[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//label[@class='custom-control-label'])[1]")).click();
+        Thread.sleep(3000);
+        String radioButtonMessageResult = driver.findElement(By.tagName("p")).getText().trim();
+        System.out.println(radioButtonMessageResult);
+        // exemplu de validare simpla
+        boolean isYes = radioButtonMessageResult.equals("You have selected Yes");
+        System.out.println("Result is " + isYes);
+
+
+
         // selectori
         // ID -> #id
         // css -> numeClasei (daca sunt spatii libere se inlocuiesc cu .)
@@ -20,14 +63,15 @@ public class Selenium {
         // Xpath -> //tag[@atribut='valoare']
         // ex xpath: //a[@target='_blank'],//div[@class='card mt-4 top-card'])[2]
         // tagname -> tagName
-        WebElement forms = driver.findElement(By.xpath("(//div[@class='card mt-4 top-card'])[2]"));
-        WebElement footer = driver.findElement(By.tagName("footer"));
-        String footerMsg = footer.getText();
 
-        System.out.println(footer.getText());
+        // WebElement forms = driver.findElement(By.xpath("(//div[@class='card mt-4 top-card'])[2]"));
+        // WebElement footer = driver.findElement(By.tagName("footer"));
+        // String footerMsg = footer.getText();
+
+        // System.out.println(footer.getText());
              //   forms.click();
-                driver.quit();
+        // driver.quit();
 
-                //driver.quit();
+        //  driver.quit();
     }
 }
--- src/test/java/org/skillbrain/emagTest.java
+++ src/test/java/org/skillbrain/emagTest.java
@@ -1,5 +1,8 @@
 package org.skillbrain;
 
-public class emagTest {
-    
-}
+
+import org.openqa.selenium.WebDriver;
+
+public class emagTest extends Utils {
+    public static void main(String[] args) {
+        WebDriver driver = getChromeDriver();
